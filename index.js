bec13c15dc590a8210da810a32bd045471af1cde
alanfm <alan.freire.87@gmail.com>
2021-01-18 15:02:01 -0300

    Meu quinto commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var readline = require('readline')
+/*var readline = require('readline')
 resp = ''
 
 var leitor = readline.createInterface({
@@ -10,4 +10,14 @@ leitor.question('Qual módulo ler dados no node.js?\n', function(answer) {
     resp = answer
     console.log('\nSua responsta "' + resp + '" foi gravada com sucesso numa váriável inútil')
     leitor.close()
-})
+})*/
+
+console.log('Digite algo');
+var variable
+process.stdin.on('readable', () => { 
+    variable = process.stdin.read(); 
+    variable = variable.toString().replace(/\n/, "")
+    variable = variable.replace(/\r/, "")
+    
+    console.log(`Você escreveu: ${variable}`)
+});
