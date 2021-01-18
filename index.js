ef18402f92c77131b9b33f70b1c7e60468f804b2
alanfm <alan.freire.87@gmail.com>
2021-01-18 08:22:13 -0300

    Meu primeiro commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var readline = require('readline')
+resp = ''
+
+var leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+leitor.question('Qual módulo ler dados no node.js?\n', function(answer) {
+    resp = answer
+    console.log('\nSua responsta "' + resp + '" foi gravada com sucesso numa váriável inútil')
+    leitor.close()
+})
+
+console.log(resp)
