9b142850aab11752e3b6a79bbef8f2da2ce2fb0d
alanfm <alan.freire.87@gmail.com>
2021-02-03 11:47:07 -0300

    Uso do pacote readline-sync

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-/*var readline = require('readline')
-resp = ''
+const reader = require('readline-sync')
 
-var leitor = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+async function lineHorizontal() {
+    console.log('------------------------')
+}
 
-leitor.question('Qual módulo ler dados no node.js?\n', function(answer) {
-    resp = answer
-    console.log('\nSua responsta "' + resp + '" foi gravada com sucesso numa váriável inútil')
-    leitor.close()
-})*/
+console.log('System of Authentication')
+lineHorizontal()
+let username = reader.question('Username: ')
+const password = reader.question('Password: ', {hideEchoBack: true})
 
-console.log('Digite algo');
-var variable
-process.stdin.on('readable', () => { 
-    variable = process.stdin.read(); 
-    variable = variable.toString().replace(/\n/, "")
-    variable = variable.replace(/\r/, "")
-    
-    console.log(`Você escreveu: ${variable}`)
-});
+lineHorizontal()
+if (username == 'admin' && password == 'foobar') {
+    console.log(`Welcome ${username}!`)
+} else {
+    console.log('User and Password not found!')
+}
+lineHorizontal()
